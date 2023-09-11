96ac90a5778f751faca22519ea5345e909015188
matriza1003 <matriza1996@gmail.com>
2023-09-11 22:24:06 +0300

    EDIT SNIPET

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -1,5 +1,9 @@
 from django.forms import ModelForm, TextInput, Textarea
 from MainApp.models import Snippet
+from django.contrib.auth.models import User
+from django.forms import CharField, PasswordInput
+from django.core.exceptions import ValidationError
+
 
 class SnippetForm(ModelForm):
     class Meta:
@@ -11,3 +15,26 @@ class SnippetForm(ModelForm):
             "name": TextInput(attrs={'placeholder': "Название сниппета"}),
             "code":Textarea(attrs={'placeholder': "Код сниппета"})
         }
+
+
+class UserRegistrationForm(ModelForm):
+   class Meta:
+    model = User
+    fields = ["username", "email"]
+
+   password1 = CharField(label="password", widget=PasswordInput)
+   password2 = CharField(label="password confirm", widget=PasswordInput)
+
+   def clean_password2(self):
+       pass1 = self.cleaned_data.get("password1")
+       pass2 = self.cleaned_data.get("password2")
+       if pass1 and pass2 and pass1 == pass2:
+           return pass2
+       raise ValidationError("Пароли не совпадают или пустые")
+
+   def save(self, commit=True):
+       user = super().save(commit=False)
+       user.set_password(self.cleaned_data["password1"])
+       if commit:
+           user.save()
+       return user
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,10 @@
-from django.http import Http404
+from django.http import Http404,HttpResponseRedirect
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
 from MainApp.forms import SnippetForm
+from django.contrib import auth
+from django.shortcuts import redirect
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def index_page(request):
@@ -31,7 +34,8 @@ def snippets_list(request):
 def snippets_page(request,value):
     x = Snippet.objects.get(id = value)
     context = {
-        "item":x
+        "item":x,
+        'type':"view"
     }
     return render(request,"pages/snipett_page.html",context)
 
@@ -42,4 +46,48 @@ def snippets_create(request):
         code = request.POST["code"]
         snippet = Snippet(name=name,lang=lang,code=code)
         snippet.save()
+        return redirect('list')
+    
+    
+def snippets_delete(request,value):
+    x = Snippet.objects.get(id = value)
+    x.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+def log_out(request):
+    auth.logout(request)
+    return redirect('home')
+
+def login_page(request):
+   if request.method == 'POST':
+       username = request.POST.get("username")
+       password = request.POST.get("password")
+       # print("username =", username)
+       # print("password =", password)
+       user = auth.authenticate(request, username=username, password=password)
+       if user is not None:
+           auth.login(request, user)
+       else:
+           # Return error message
+           pass
+   return redirect('home')
+
+def snippets_edit(request,value):
+    try:
+        snipet = Snippet.objects.get(id=value)
+    except ObjectDoesNotExist:
+        raise Http404
+    if request.method == "GET":
+
+        context = {
+            "item":snipet,
+            'type' :"edit"
+        }
+        return render(request,"pages/snipett_page.html",context)
+    else:
+        form_date = request.POST
+        snipet.name = form_date["name"]
+        snipet.code = form_date['code']
+        snipet.creation_date = form_date["creation_date"]
+        snipet.save()
         return redirect('list')
